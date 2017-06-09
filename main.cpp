49143f9fc03cc7fcdaae21e2c631e24b1c50baee
Jackson Singleton <jackson.singleton1@gmail.com>
2017-06-09 14:32:23 -0500

    Fixed sigfig conversion. Reduced code by a few lines.

--- main.cpp
+++ main.cpp
@@ -1,40 +1,27 @@
 #include <iostream>
 using namespace std;
-bool healthMill;
 class Monk{
     int baseHealth = 365000;
     int agility, stamina;
     string specialization, monkName;
 public:
-    double health(){
-        if(baseHealth * (double(stamina/6000)) > 1000000) {
-            healthMill = true;
-            return (baseHealth * (double(stamina / 6000)))/1000000;
-        }
-        else {
-            healthMill = false;
-            return baseHealth * (double(stamina / 6000));
-        }
-    }
+    double health(){ return baseHealth * (stamina * 0.0012); }
     double damage(){return agility * 2.4;}
+
     string specName(){return specialization;}
     Monk(int, int, string, string);
 
 };
 
 Monk::Monk(int stam, int agil, string spec, string name):stamina(stam),
-            agility(agil),specialization(spec),monkName(name){
-    if (healthMill) {
-        cout << name << "'s Health is " << health() << " million." << endl;
-    }
-    else{
-        cout << name << "'s Health is " << health() << "." << endl;
-    }
-    cout << name << "'s DPS is " << damage() << "."<< endl;
+            agility(agil),specialization(spec),monkName(name)
+{
+    if(health() > 1000000){cout << name << "'s Health is " << (health()/1000000) << " million HP." << endl;}
+    else{cout << name << "'s Health is " << health() << " HP." << endl;}
+    cout << name << "'s DPS is " << damage() << " damage per second."<< endl;
     cout << name << "'s current specification is a " << specName() << " monk." << endl;
 }
 int main() {
-    Monk Tyler(25000, 1245, "Brewmaster", "Tyler");
-    Monk Jackson(12500, 3000, "Windwalker", "Jackson");
+    Monk Schwifty1sh0t(5000, 1263, "Windwalker", "Schwifty1sh0t");
     return 0;
 }
